15c4ecb6f71f89f46fd7e580c8c91c9279b6e303
Yuan Chiu <chyuaner@gmail.com>
2026-05-23 01:49:36 +0800

    feat: add setFullscreen plugin method to control system UI visibility in ThemeControlPlugin

--- android/app/src/main/java/tw/yuaner/capacitor_elrc_maker/ThemeControlPlugin.java
+++ android/app/src/main/java/tw/yuaner/capacitor_elrc_maker/ThemeControlPlugin.java
@@ -51,6 +51,55 @@ public class ThemeControlPlugin extends Plugin {
         }
     }
 
+    @PluginMethod
+    public void setFullscreen(PluginCall call) {
+        final Boolean fullscreen = call.getBoolean("fullscreen");
+        if (fullscreen == null) {
+            call.reject("fullscreen parameter is required");
+            return;
+        }
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Window window = getActivity().getWindow();
+                    if (fullscreen) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            window.getInsetsController().hide(
+                                android.view.WindowInsets.Type.statusBars() | 
+                                android.view.WindowInsets.Type.navigationBars()
+                            );
+                            window.getInsetsController().setSystemBarsBehavior(
+                                android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                            );
+                        } else {
+                            int flags = android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                            window.getDecorView().setSystemUiVisibility(flags);
+                        }
+                    } else {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                            window.getInsetsController().show(
+                                android.view.WindowInsets.Type.statusBars() | 
+                                android.view.WindowInsets.Type.navigationBars()
+                            );
+                        } else {
+                            int flags = android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                            window.getDecorView().setSystemUiVisibility(flags);
+                        }
+                    }
+                    call.resolve();
+                } catch (Exception e) {
+                    call.reject("Failed to set fullscreen: " + e.getMessage());
+                }
+            }
+        });
+    }
+
     private boolean isColorDark(int color) {
         double darkness = 1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255;
         return darkness >= 0.5;
